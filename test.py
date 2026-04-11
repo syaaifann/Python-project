48c70626dadd3d0982afaf5e0e987694c8dd0902
syaaifann <ifanadikusuma2608@gmail.com>
2026-04-12 01:43:24 +0700

    project pertama

---
+++ test.py
@@ -0,0 +1,7 @@
+nama = input("masukan nama kamu: ")
+id = int(input("masukan id kamu: "))
+levelAkses = input("masukan level akses kamu: ")
+
+print("user", nama)
+print("dengan id", id)
+print("telah terdaftar", levelAkses)
